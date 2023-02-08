27ce17dea05723482cb410d1e59b9c855d342e02
Matthew <mchen146@ucsc.edu>
2023-02-07 20:05:53 -0800

    working on saving cursor selection/position

--- wwwroot/index.js
+++ wwwroot/index.js
@@ -1,37 +1,3 @@
-const callBySelectorMap = new Map();
-
-window.callBySelector = (selector, callbackString, args) => {
-    let element = callBySelectorMap.get(selector);
-    if (!element) {
-        element = document.querySelector(selector);
-        if (element) {
-            callBySelectorMap.set(selector, element);
-        } else {
-            console.log('couldnt find element by selector', selector);
-            return;
-        }
-    }
-    const callback = new Function('element', 'args', callbackString);
-    if (typeof(callback) == 'function') {
-        return callback(element, args);
-    }
-};
-
-const textArea = document.querySelector('.script'); 
-if (!textArea) {
-    console.error('could not find script editor');
-}
-
-window.scriptTextArea = textArea;
-
-const scriptView = document.querySelector('.script-view');
-
-window.setScriptViewInnerHTML = (text) => {
-    if (scriptView) {
-        scriptView.innerHTML = text;
-    }
-}
-
 const log = document.querySelector('.log');
 
 window.addLogInnerHTML = (text) => {
@@ -41,46 +7,48 @@ window.addLogInnerHTML = (text) => {
     }
 }
 
-const scriptContainer = document.querySelector('.script-container');
 
-window.resizeView = function (setViewContent = true) {
-    const scrollTop = scriptContainer.scrollTop;
+async function parse(text) {
+    
+    const result = await window.callDotNetReferenceMethod('index', 'Parse', text);
 
-    textArea.style.height = '';
-    textArea.style.height = `${textArea.scrollHeight}px`;
+    return result;
 
-    if (scriptView) {
-        const rect = textArea.getBoundingClientRect();
-        //scriptView.style.left = rect.x;
-        //scriptView.style.top = rect.y;
-        scriptView.style.width = `${rect.width}px`;
-        scriptView.style.height = `${rect.height}px`;
-
-        if (setViewContent) {
-            scriptView.textContent = textArea.value;
-        }
-    }
-
-    scriptContainer.scrollTop = scrollTop;
 }
 
-window.updateScript = function () {
-    console.debug('calling update script');
-    window.callDotNetReferenceMethod('index', 'UpdateScript', textArea.value);
+let scriptCheckTimeout;
+
+const scriptEditor = document.querySelector('.script');
+
+async function updateScript() {
+    console.log('updating script');
+
+    // send script textcontent to parser to parse
+    const result = await parse(scriptEditor.textContent);
+
+    const selection = document.getSelection();
+    
+    const selectionLength = selection.toString().length;
+
+    selection.modify('extend', 'backward', 'paragraphboundary');
+
+    
+
+
+    console.log(selection);
+    const range = selection.getRangeAt(0);
+    selection.removeAllRanges();
+
+    scriptEditor.innerHTML = result.innerHTML;
+
+    selection.addRange(range);
+    console.log('new', selection);
+
 }
 
-let scriptCheckTimeout = setTimeout(updateScript, 500);
+scriptEditor?.addEventListener('input', () => {
 
-setTimeout(() => {
-    resizeView();
-    updateScript();
-}, 1);
-
-window.addEventListener('resize', resizeView.bind(this, false));
-
-textArea.addEventListener('input', () => {
     clearTimeout(scriptCheckTimeout);
     scriptCheckTimeout = setTimeout(updateScript, 100);
 
-    resizeView();
 });
